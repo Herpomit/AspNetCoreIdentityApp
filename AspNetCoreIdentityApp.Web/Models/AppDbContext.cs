46cee63001dc725f962d4cfc54fa428a275d6cd2
Herpomit <canberkakin8@gmail.com>
2023-09-01 13:29:54 +0300

    Add project files.

---
+++ AspNetCoreIdentityApp.Web/Models/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetCoreIdentityApp.Web.Models
+{
+    public class AppDbContext : IdentityDbContext<AppUser, AppRole, int>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> opt):base(opt) { }
+    }
+}
